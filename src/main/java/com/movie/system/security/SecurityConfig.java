f4dbe5108b359723a818d7dd4c29ceefd48cc8d3
Luis <1240904@isep.ipp.pt>
2026-03-22 01:13:06 +0000

    update cors configuration

--- src/main/java/com/movie/system/security/SecurityConfig.java
+++ src/main/java/com/movie/system/security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://moviereservation-1.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
